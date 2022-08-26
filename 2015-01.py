fe0bbb97089b3a52f20d9da8ceffd8e61e43ed55
Henrik <henriks@gmail.com>
2022-08-26 10:02:00 +0200

    Day 1 2015 part 2 solved

--- 2015-01.py
+++ 2015-01.py
@@ -10,12 +10,18 @@ def main():
 
 def run(s):
     floor = 0
+    firstTimeInBasement = 0
+    moves = 0
     for c in s:
+        moves += 1
         if c == '(':
             floor += 1
         else:
             floor -= 1
+            if firstTimeInBasement == 0 and floor < 0:
+                firstTimeInBasement = moves
     print(floor)
+    print(firstTimeInBasement)
 
 
 def runReal():
