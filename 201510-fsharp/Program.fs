8a85fed233a7287a8ae99333656594e2da924505
Henrik <henriks@gmail.com>
2022-09-07 17:29:16 +0200

    Done!

--- 201510-fsharp/Program.fs
+++ 201510-fsharp/Program.fs
@@ -1,62 +1,65 @@
 ﻿open System
+open System.Text.RegularExpressions
 
 type Part =
     struct
-        val character: char
-        val count: int
+        val Character: char
+        val Count: int
+        new(character: char, count: int) = { Character = character; Count = count }
     end
 
-// let GenerateString (part: Part) = $"{part.count}{part.character}"
+let rec generateParts (s: string) lastchar charcount =
+    let length = String.length s
 
-// let GenerateStringFromList parts =
-//     let s = ""
-//     let strings = List.iter (GenerateString) parts
+    if length = 0 then
+        seq { yield new Part(lastchar, charcount) }
+    else
+        let currentchar = s[0]
 
-//     String.concat "" strings
+        seq {
+            if currentchar <> lastchar then
+                yield new Part(lastchar, charcount)
+                yield! generateParts s[1..] currentchar 1
+            else
+                yield! generateParts s[1..] currentchar (charcount + 1)
+        }
 
-// let GetParts s = [ new Part('a', 0) ]
+let createPart (s: string) =
+    let c = s[0]
+    let l = String.length s
+    new Part(c, l)
 
-let NextString s iterations iteration = s
-// if iteration >= iterations then
-//     s
-// else
-//     NextString(GenerateStringFromList(GetParts(s)), iterations, iteration + 1)
+let getParts (s: string) =
+    let regex = new Regex("(.)\\1*")
+    let matches = regex.Matches s
+    let strings = Seq.map (fun x -> x.ToString()) matches
+    Seq.map (createPart) strings
 
-let LookAndSayGame input iterations = NextString input iterations 0
+let generateString (part: Part) = $"{part.Count}{part.Character}"
 
-let GetParts s = s |> Seq.countBy id
+let generateStringFromList parts =
+    let s = ""
+    let strings = Seq.map (generateString) parts
 
-// let GetParts s
-//     let stringlength = String.length s
-//     let looplength = stringlength - 1
-//     seq{
-//         for i in 0..looplength do
-//             let c = s[i]
-//             for j in i..looplength
+    String.concat "" strings
 
-//     }
-// private IList<Part> GetParts(string s)
-// {
-//     var parts = new List<Part>();
-//     Part currentPart = null;
-//     foreach (var c in s)
-//     {
-//         if (currentPart == null || c != currentPart.Character)
-//         {
-//             currentPart = new Part(c);
-//             parts.Add(currentPart);
-//         }
+let rec nextString (s: string) (iterations: int) (iteration: int) : string =
+    if iteration >= iterations then
+        s
+    else
+        let parts = getParts s
+        let newString = generateStringFromList parts
+        let nextIteration = iteration + 1
+        nextString newString iterations nextIteration
 
-//         currentPart.Count++;
-//     }
+let lookAndSayGame input iterations = nextString input iterations 0
 
-//     return parts;
-// }
+let input = "1113222113"
 
-//let result = LookAndSayGame "1113222113" 40
-//printfn "%s" result
+let result1 = lookAndSayGame input 40
+let length1 = String.length result1
+printfn "%A" length1
 
-let r = GetParts "1113222113"
-let last = List.ofSeq r
-let (a, b) = last
-printfn a
+let result2 = lookAndSayGame input 50
+let length2 = String.length result2
+printfn "%A" length2
