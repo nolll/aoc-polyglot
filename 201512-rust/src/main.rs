459edb889c073f9e0dae65cf3ae5f1d1eb04878c
Henrik <henriks@gmail.com>
2022-09-09 17:45:52 +0200

    part 2

--- 201512-rust/src/main.rs
+++ 201512-rust/src/main.rs
@@ -4,18 +4,16 @@ use std::fs;
 
 fn main() {
     let input = read_input();
-    //let input = "{ \"test\": 1, \"apa\": 2}".to_string();
     let json = parse_json(input);
-    //println!("{}", json);
     
-    let sum1 = get_recursive_sum(json);
+    let sum1 = get_recursive_sum(json.clone(), true);
     println!("{}", sum1);
-    
-    // let t = get_type(json);
-    // println!("{}", t);
+
+    let sum2 = get_recursive_sum(json.clone(), false);
+    println!("{}", sum2);
 }
 
-fn get_recursive_sum(data: serde_json::Value) -> i64 {
+fn get_recursive_sum(data: serde_json::Value, include_red: bool) -> i64 {
     match data{
         serde_json::Value::Number(d) => {
             let result = d.as_i64();
@@ -29,17 +27,26 @@ fn get_recursive_sum(data: serde_json::Value) -> i64 {
         }
         serde_json::Value::Object(d) => {
             let mut sum = 0;
+            let mut has_red = false;
 
             for (_key, value) in d{
-                sum += get_recursive_sum(value);
+                if value == "red" {
+                    has_red = true;
+                }
+                sum += get_recursive_sum(value, include_red);
             }
+
+            if !include_red && has_red {
+                return 0;
+            }
+
             return sum;
         }
         serde_json::Value::Array(d) => {
             let mut sum = 0;
 
             for i in 0..(d.len()) {
-                sum += get_recursive_sum(d[i].clone());
+                sum += get_recursive_sum(d[i].clone(), include_red);
             }
             return sum;
         }
@@ -57,28 +64,4 @@ fn parse_json(input: String) -> serde_json::Value {
 fn read_input() -> String {
     let s = fs::read_to_string("input.txt").expect("file error");
     return s;
-    // let mut file = File::open("input.txt").unwrap();
-    // let mut stdout = stdout();
-    // let str = &copy(&mut file, &mut stdout).unwrap().to_string();
-    // return str.to_string();
 }
-
-// fn get_type(data: serde_json::Value) -> String {
-//     match data{
-//         serde_json::Value::Number(_) => {
-//             return "Integer".to_string();
-//         }
-//         serde_json::Value::String(_) => {
-//             return "String".to_string();
-//         }
-//         serde_json::Value::Object(_) => {
-//             return "Object".to_string();
-//         }
-//         serde_json::Value::Array(_) => {
-//             return "Array".to_string();
-//         }
-//         _ => {
-//             return "Unknown".to_string();
-//         }
-//     }
-// }
