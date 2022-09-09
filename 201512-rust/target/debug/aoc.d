8e3664cd369baa1578cfe80ceb8d258bc06f4130
Henrik <henriks@gmail.com>
2022-09-09 14:18:13 +0200

    rust hello world

---
+++ 201512-rust/target/debug/aoc.d
@@ -0,0 +1,1 @@
+C:\dev\aoc-polyglot\201512-rust\target\debug\aoc.exe: C:\dev\aoc-polyglot\201512-rust\src\main.rs
---
+++ 201512-rust/target/debug/deps/aoc-a97dd502709fb9dc.d
@@ -0,0 +1,5 @@
+C:\dev\aoc-polyglot\201512-rust\target\debug\deps\aoc-a97dd502709fb9dc.exe: src\main.rs
+
+C:\dev\aoc-polyglot\201512-rust\target\debug\deps\aoc-a97dd502709fb9dc.d: src\main.rs
+
+src\main.rs:
