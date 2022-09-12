5d4e049d1a3864befb66e11849f480a3b2da22f2
Henrik <henriks@gmail.com>
2022-09-12 16:38:18 +0200

    Parsing dinner table

--- 201513-erlang/aoc.erl
+++ 201513-erlang/aoc.erl
@@ -1,82 +1,85 @@
 -module(aoc).
 -export([run/0]).
 
-readfile(FileName) ->
+readlines(FileName) ->
     {ok, Binary} = file:read_file(FileName),
     string:tokens(erlang:binary_to_list(Binary), "\n").
 
-createGuest(Name) ->
+parseValue(StringValue) ->
+    {IntValue, _} = string:to_integer(StringValue),
+    IntValue.
+
+parseAction(StringAction) ->
+    IsGain = string:equal(StringAction, "gain"),
+    if
+        IsGain ->
+            gain;
+        true ->
+            lose
+    end.
+
+getMultiplier(gain) ->
+    1;
+getMultiplier(lose) ->
+    -1.
+
+createGuest(Name, Rules) ->
     #{
         name => Name,
-        rules => []
+        rules => Rules
     }.
 
+createRule(Name, Happiness) ->
+    #{
+        name => Name,
+        happiness => Happiness
+    }.
+
+getGuest(Name, Guests) ->
+    Exists = maps:is_key(Name, Guests),
+    if
+        Exists -> maps:get(Name, Guests);
+        true -> createGuest(Name, [])
+    end.
+
+parseGuest(Row, Guests) ->
+    S1 = string:trim(Row),
+    S2 = string:trim(S1, trailing, "."),
+    Parts = re:split(S2, " "),
+    [Name, _, StringAction, StringValue, _, _, _, _, _, _, OtherName | _] = Parts,
+
+    Action = parseAction(StringAction),
+    Multiplier = getMultiplier(Action),
+    Happiness = parseValue(StringValue) * Multiplier,
+
+    Guest = getGuest(Name, Guests),
+    Rules = maps:get(rules, Guest),
+    Rule = createRule(OtherName, Happiness),
+    createGuest(Name, lists:append(Rules, [Rule])).
+
 run() ->
-    Lines = readfile("input.txt"),
-    Guests = lists:map(
-        fun(Name) ->
-            createGuest(Name)
-        end,
-        Lines
-    ),
-    GuestCount = length(Guests),
-    io:fwrite("guests: ~w~n", [GuestCount]).
+    Rows = readlines("input.txt"),
+    Guests = parseGuests(Rows, false),
+    Keys = maps:keys(Guests),
+    Length = length(Keys),
+    io:fwrite("guests: ~w~n", [Length]).
 
-% parseGuests(input, includeMe) ->
-%     Rules = readInput(input).
-%     Guests = new Dictionary<string, DinnerGuest>().
+parseGuests(Rows, IncludeMe) ->
+    InitialGuests = createGuestMap(IncludeMe),
+    parseNextGuest(Rows, InitialGuests).
 
-% if (includeMe)
-% {
-%     const string name = "Me";
-%     guests.Add(name, new DinnerGuest(name));
-% }
+parseNextGuest([], Guests) ->
+    Guests;
+parseNextGuest(Rows, Guests) ->
+    [First | TheRest] = Rows,
+    Guest = parseGuest(First, Guests),
+    Name = maps:get(name, Guest),
+    parseNextGuest(TheRest, maps:put(Name, Guest, Guests)).
 
-% foreach (var r in rules)
-% {
-%     var parts = r.TrimEnd('.').Split(' ');
-%     var name = parts[0];
-%     var sign = parts[2] == "lose" ? -1 : 1;
-%     var happiness = sign * int.Parse(parts[3]);
-%     var otherName = parts[10];
-%     var rule = new DinnerGuestRule(otherName, happiness);
-%     if (!guests.TryGetValue(name, out var guest))
-%     {
-%         guest = new DinnerGuest(name);
-%         guests.Add(name, guest);
-%     }
-
-%     guest.AddRule(rule);
-% }
-
-% Guests.
-
-% dinnerTable(input, includeMe) ->
-%     Guests = parseGuests(input, includeMe).
-%     var names = guests.Keys;
-%     var nameLists = PermutationGenerator.GetPermutations(names.ToList());
-%     var happiness = nameLists.Select(nl => CalculateHappiness(nl.Select(p => guests[p]).ToList()));
-
-%     HappinessChange = happiness.Max();
-
-% calculateHappiness(guests) ->
-%     var happiness = 0;
-%     for (var i = 0; i < guests.Count; i++)
-%     {
-%         var guest = guests[i];
-%         var nextGuestIndex = i + 1 >= guests.Count ? 0 : i + 1;
-%         var prevGuestIndex = i - 1 < 0 ? guests.Count - 1 : i - 1;
-%         var nextGuest = guests[nextGuestIndex];
-%         var prevGuest = guests[prevGuestIndex];
-%         var nextGuestHappiness = guest.GetHappiness(nextGuest.Name);
-%         var prevGuestHappiness = guest.GetHappiness(prevGuest.Name);
-%         happiness += nextGuestHappiness + prevGuestHappiness;
-%     }
-%     return happiness;
-
-% getPermutations(list, length) ->
-%     if (length == 1) return list.Select(t => new[] { t });
-
-%     return GetPermutations(list, list.length - 1)
-%         .SelectMany(t => list.Where(e => !t.Contains(e)),
-%             (t1, t2) => t1.Concat(new[] { t2 }));
+createGuestMap(IncludeMe) ->
+    if
+        IncludeMe ->
+            #{name => "Me", rules => []};
+        true ->
+            #{}
+    end.
