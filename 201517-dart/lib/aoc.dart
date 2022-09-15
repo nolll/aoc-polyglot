586e44bc3d5c66545a8999d4653f07bcfe0cfdb1
Henrik <henriks@gmail.com>
2022-09-15 13:27:17 +0200

    cleanup

--- 201517-dart/lib/aoc.dart
+++ 201517-dart/lib/aoc.dart
@@ -3,42 +3,57 @@ import 'dart:convert';
 
 run() async {
   var rows = await readLines();
-  var containers = EggnogContainers(rows);
+  var containers = parseContainers(rows);
 
-  var part1Combinations = containers.getCombinations(150);
+  var part1Combinations = getCombinations(containers, 150);
   print(part1Combinations.length);
 
-  var part2Combinations = containers.getCombinationsWithLeastContainers(150);
+  var part2Combinations = getCombinationsWithLeastContainers(containers, 150);
   print(part2Combinations.length);
 }
 
-class EggnogContainers {
-  late List<EggnogContainer> _containers;
-  CombinationGenerator generator = CombinationGenerator();
+List<EggnogContainer> parseContainers(List<String> rows) {
+  return rows.asMap().entries.map((entry) {
+    return EggnogContainer(entry.key, int.parse(entry.value));
+  }).toList();
+}
 
-  EggnogContainers(List<String> rows) {
-    _containers = rows.asMap().entries.map((entry) {
-      return EggnogContainer(entry.key, int.parse(entry.value));
-    }).toList();
+List<List<EggnogContainer>> getCombinations(List<EggnogContainer> containers, int targetVolume) {
+  var combinations = getAllCombinations(containers);
+  return combinations.where((o) => getSum(o) == targetVolume).toList();
+}
+
+int getSum(List<EggnogContainer> combination) {
+  return combination.map((o) => o.volume).reduce((value, element) => value + element);
+}
+
+List<List<EggnogContainer>> getCombinationsWithLeastContainers(List<EggnogContainer> containers, int targetVolume) {
+  var combinations = getCombinations(containers, targetVolume);
+  combinations.sort((a, b) => a.length.compareTo(b.length));
+  var smallestCount = combinations.first.length;
+  return combinations.where((o) => o.length == smallestCount).toList();
+}
+
+List<List<EggnogContainer>> getAllCombinations<T>(List<EggnogContainer> list) {
+  var result = <List<EggnogContainer>>[];
+  result.add(<EggnogContainer>[]);
+  result.last.add(list[0]);
+  if (list.length == 1) {
+    return result;
   }
-
-  List<List<EggnogContainer>> getCombinations(int targetVolume) {
-    var combinations = generator.getAllCombinations(_containers);
-    return combinations.where((o) => getSum(o) == targetVolume).toList();
+  var tailCombos = getAllCombinations(list.skip(1).toList());
+  for (var combo in tailCombos) {
+    result.add(List<EggnogContainer>.from(combo));
+    combo.add(list[0]);
+    result.add(List<EggnogContainer>.from(combo));
   }
+  return result;
+}
 
-  getSum(List<EggnogContainer> combination) {
-    return combination.map((o) => o.volume).reduce((value, element) => value + element);
-  }
-
-  List<List<EggnogContainer>> getCombinationsWithLeastContainers(int targetVolume) {
-    var combinations = getCombinations(targetVolume);
-    combinations.sort((a, b) => a.length.compareTo(b.length));
-    var smallestCount = combinations.first.length;
-    return combinations.where((o) => o.length == smallestCount).toList();
-  }
-
-  parseContainers() {}
+Future<List<String>> readLines() async {
+  var s = await File('./resources/input.txt').readAsString();
+  var lineSplitter = LineSplitter();
+  return lineSplitter.convert(s);
 }
 
 class EggnogContainer {
@@ -60,28 +75,4 @@ class EggnogContainer {
     }
     return other is EggnogContainer && other.id == id;
   }
-}
-
-class CombinationGenerator {
-  List<List<EggnogContainer>> getAllCombinations<T>(List<EggnogContainer> list) {
-    var result = <List<EggnogContainer>>[];
-    result.add(<EggnogContainer>[]);
-    result.last.add(list[0]);
-    if (list.length == 1) {
-      return result;
-    }
-    var tailCombos = getAllCombinations(list.skip(1).toList());
-    for (var combo in tailCombos) {
-      result.add(List<EggnogContainer>.from(combo));
-      combo.add(list[0]);
-      result.add(List<EggnogContainer>.from(combo));
-    }
-    return result;
-  }
-}
-
-Future<List<String>> readLines() async {
-  var s = await File('./resources/input.txt').readAsString();
-  var lineSplitter = LineSplitter();
-  return lineSplitter.convert(s);
 }
