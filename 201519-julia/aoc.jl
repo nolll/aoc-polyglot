a2c4ca504756de4a1d5070a1307244b3064d364b
Henrik <henriks@gmail.com>
2022-09-17 19:27:35 +0200

    no errors for part 1, but wring answer

--- 201519-julia/aoc.jl
+++ 201519-julia/aoc.jl
@@ -1,70 +1,85 @@
-print("Hello World")
+import Base: isless
 
-_replacements = PuzzleInputReader.ReadLines(input).Select(ParseReplacement).OrderByDescending(o => o.Right.Length).ThenBy(o => o.Right)
-
-getCalibrationMolecules(startMolecule)
-    molecules = string[]
-    for replacement in _replacements
-        molecules.AddRange(replacement.Expand(startMolecule))
+function getCalibrationMolecules(startMolecule, replacements)
+    molecules = String[]
+    for replacement in replacements
+        newMolecules = expand(startMolecule, replacement)
+        molecules = cat(molecules, newMolecules, dims=1)
     end
-    molecules.Distinct().ToList()
+    unique(molecules)
 end
 
-stepsToMake(molecule)
-    steps = 0
-    while molecule != "e"
-        for replacement in _replacements
-            pos = molecule.IndexOf(replacement.Right, StringComparison.InvariantCulture)
-            if (pos >= 0)
-            {
-                molecule = ReplaceFirst(molecule, replacement.Right, replacement.Left)
-                steps++
-                break
-            }
-        end
+# function stepsToMake(molecule)
+#     steps = 0
+#     while molecule != "e"
+#         for replacement in _replacements
+#             pos = molecule.IndexOf(replacement.Right, StringComparison.InvariantCulture)
+#             if (pos >= 0)
+#                 molecule = replaceFirst(molecule, replacement.Right, replacement.Left)
+#                 steps++
+#                 break
+#             end
+#         end
+#     end
+
+#     steps
+# end
+
+# function replaceFirst(text, search, replace)
+#     pos = text.IndexOf(search, StringComparison.InvariantCulture)
+#     if pos < 0
+#         return text
+#     end
+#     string.Concat(text.Substring(0, pos), replace, text.Substring(pos + search.Length))
+# end
+
+function parseReplacement(s)
+    parts = split(s, " => ")
+    input = parts[1]
+    output = parts[2]
+    MoleculeReplacement(input, output)
+end
+
+function expand(inputMolecule, replacement)
+    molecules = String[]
+    staticParts = split(inputMolecule, replacement.left)
+    if length(staticParts) < 2
+        return String[]
     end
 
-    steps
-end
-
-replaceFirst(text, search, replace)
-    pos = text.IndexOf(search, StringComparison.InvariantCulture)
-    if pos < 0
-        return text
-    string.Concat(text.Substring(0, pos), replace, text.Substring(pos + search.Length))
-end
-
-parseReplacement(s)
-    parts = s.Split(" => ")
-    input = parts[0]
-    output = parts[1]
-    new MoleculeReplacement(input, output)
-end
-
-expand(inputMolecule, replacement)
-    molecules = string[]
-    staticParts = inputMolecule.Split(Left)
-    if staticParts.Length < 2
-        return string[]
-
-    numberOfReplacements = staticParts.Length - 1
-    for i = 0:numberOfReplacements
-        sb = new StringBuilder()
-        for j = 0:numberOfReplacements
-            sb.Append(staticParts[j])
-            newReplacement = i == j ? Right : Left
-            sb.Append(newReplacement)
+    numberOfReplacements = length(staticParts)
+    for i = 1:numberOfReplacements
+        s = ""
+        for j = 1:numberOfReplacements
+            s = s * staticParts[j]
+            newReplacement = i == j ? replacement.right : replacement.left
+            s = s * newReplacement
         end
 
-        sb.Append(staticParts.Last())
+        s = s * last(staticParts)
 
-        molecules.Add(sb.ToString())
+        push!(molecules, s)
     end
 
     molecules
+end
+
+function readInput()
+    file = open("input.txt", "r")
+    readlines(file)
 end
 
 struct MoleculeReplacement
     left
     right
 end
+
+isless(a::MoleculeReplacement, b::MoleculeReplacement) = isless(a.right, b.right) || isless(length(a.right), length(b.right))
+
+targetMolecule = "CRnCaCaCaSiRnBPTiMgArSiRnSiRnMgArSiRnCaFArTiTiBSiThFYCaFArCaCaSiThCaPBSiThSiThCaCaPTiRnPBSiThRnFArArCaCaSiThCaSiThSiRnMgArCaPTiBPRnFArSiThCaSiRnFArBCaSiRnCaPRnFArPMgYCaFArCaPTiTiTiBPBSiThCaPTiBPBSiRnFArBPBSiRnCaFArBPRnSiRnFArRnSiRnBFArCaFArCaCaCaSiThSiThCaCaPBPTiTiRnFArCaPTiBSiAlArPBCaCaCaCaCaSiRnMgArCaSiThFArThCaSiThCaSiRnCaFYCaSiRnFYFArFArCaSiRnFYFArCaSiRnBPMgArSiThPRnFArCaSiRnFArTiRnSiRnFYFArCaSiRnBFArCaSiRnTiMgArSiThCaSiThCaFArPRnFArSiRnFArTiTiTiTiBCaCaSiRnCaCaFYFArSiThCaPTiBPTiBCaSiThSiRnMgArCaF"
+rows = readInput()
+replacements = map(parseReplacement, rows)
+sortedReplacements = sort(replacements, rev=true) # then by right
+
+calibrationMolecules = getCalibrationMolecules(targetMolecule, replacements)
+print(length(calibrationMolecules))
