a03743412ff77d515aa9df9bef9a08602a0d0bd6
Henrik <henriks@gmail.com>
2022-09-21 15:28:52 +0200

    cleanup

--- 201521-elixir/aoc.exs
+++ 201521-elixir/aoc.exs
@@ -1,18 +1,22 @@
 defmodule RpgCharacter do
-    defstruct name: "", points: 0, damage: 0, armor: 0
+    defstruct name: :unknownCharacter, points: 0, damage: 0, armor: 0
 end
 
 defmodule RpgProperty do
-    defstruct name: "", cost: 0, damage: 0, armor: 0
+    defstruct name: :unknownProperty, cost: 0, damage: 0, armor: 0
 end
 
 defmodule Aoc do
     def run() do
-        boss = createBoss(104, 8, 1)
+        bossPoints = 104
+        bossDamage = 8
+        bossArmor = 1
+
+        boss = createBoss(bossPoints, bossDamage, bossArmor)
         propertyCombinations = getPropertyCombinations()
         lowestCostToWin = findLowest(propertyCombinations, boss)
-        IO.puts(lowestCostToWin)
         highestCostToLose = findHighest(propertyCombinations, boss)
+        IO.puts(lowestCostToWin)
         IO.puts(highestCostToLose)
     end
 
@@ -22,7 +26,7 @@ defmodule Aoc do
         [head | tail] = propertyCombinations
         player = createPlayer(head)
         winner = runGame(player, boss)
-        didPlayerWin = if winner.name == "Player", do: true, else: false
+        didPlayerWin = if winner.name == :player, do: true, else: false
         newLowest = if didPlayerWin, do: min(lowest, cost(head)), else: lowest
         findLowest(tail, boss, newLowest)
     end
@@ -33,7 +37,7 @@ defmodule Aoc do
         [head | tail] = propertyCombinations
         player = createPlayer(head)
         winner = runGame(player, boss)
-        didBossWin = if winner.name == "Boss", do: true, else: false
+        didBossWin = if winner.name == :boss, do: true, else: false
         newHighest = if didBossWin, do: max(highest, cost(head)), else: highest
         findHighest(tail, boss, newHighest)
     end
@@ -45,22 +49,18 @@ defmodule Aoc do
     def runGame(player, boss), do: attack(player, boss)
 
     def attack(attacker, defender) do
-        if(isAlive(defender)) do
-            attack(hurt(defender, attacker.damage), attacker)
-        else
-            attacker
-        end
+        if isAlive(defender), do: attack(hurt(defender, attacker.damage), attacker), else: attacker
     end
 
     def createPlayer(), do: createPlayer(0, 0)
-    def createPlayer(damage, armor), do: createRpgCharacter("Player", 100, damage, armor)
+    def createPlayer(damage, armor), do: createRpgCharacter(:player, 100, damage, armor)
     def createPlayer(properties) do
         damage = Enum.sum(Enum.map(properties, fn p -> p.damage end))
         armor = Enum.sum(Enum.map(properties, fn p -> p.armor end))
         createPlayer(damage, armor)
     end
 
-    def createBoss(points, damage, armor), do: createRpgCharacter("Boss", points, damage, armor)
+    def createBoss(points, damage, armor), do: createRpgCharacter(:boss, points, damage, armor)
 
     def createRpgCharacter(name, points, damage, armor) do
         %RpgCharacter{name: name, points: points, damage: damage, armor: armor}
@@ -68,32 +68,32 @@ defmodule Aoc do
 
     def createWeapons() do
         [
-            createRpgProperty("Dagger", 8, 4, 0),
-            createRpgProperty("Shortsword", 10, 5, 0),
-            createRpgProperty("Warhammer", 25, 6, 0),
-            createRpgProperty("Longsword", 40, 7, 0),
-            createRpgProperty("Greataxe", 74, 8, 0)
+            createRpgProperty(:dagger, 8, 4, 0),
+            createRpgProperty(:shortsword, 10, 5, 0),
+            createRpgProperty(:warhammer, 25, 6, 0),
+            createRpgProperty(:longsword, 40, 7, 0),
+            createRpgProperty(:greataxe, 74, 8, 0)
         ]
     end
 
     def createArmor() do
         [
-            createRpgProperty("Leather", 13, 0, 1),
-            createRpgProperty("Chainmail", 31, 0, 2),
-            createRpgProperty("Splintmail", 53, 0, 3),
-            createRpgProperty("Bandedmail", 75, 0, 4),
-            createRpgProperty("Platemail", 102, 0, 5)
+            createRpgProperty(:leather, 13, 0, 1),
+            createRpgProperty(:chainmail, 31, 0, 2),
+            createRpgProperty(:splintmail, 53, 0, 3),
+            createRpgProperty(:bandedmail, 75, 0, 4),
+            createRpgProperty(:platemail, 102, 0, 5)
         ]
     end
 
     def createRings() do
         [
-            createRpgProperty("Damage +1", 25, 1, 0),
-            createRpgProperty("Damage +2", 50, 2, 0),
-            createRpgProperty("Damage +3", 100, 3, 0),
-            createRpgProperty("Defense +1", 20, 0, 1),
-            createRpgProperty("Defense +2", 40, 0, 2),
-            createRpgProperty("Defense +3", 80, 0, 3)
+            createRpgProperty(:damage1, 25, 1, 0),
+            createRpgProperty(:damage2, 50, 2, 0),
+            createRpgProperty(:damage3, 100, 3, 0),
+            createRpgProperty(:defense1, 20, 0, 1),
+            createRpgProperty(:defense2, 40, 0, 2),
+            createRpgProperty(:defense3, 80, 0, 3)
         ]    
     end
 
@@ -111,11 +111,7 @@ defmodule Aoc do
 
     def getDamage(attack, armor) do
         damage = attack - armor
-        if(damage > 0) do
-            damage
-        else
-            1
-        end
+        if damage > 0, do: damage, else: 1
     end
 
     def getPropertyCombinations() do
