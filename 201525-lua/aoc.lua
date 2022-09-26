cdb372ab465c3a2f07324aa2c3ad90eed582da46
Henrik <henriks@gmail.com>
2022-09-26 12:09:19 +0200

    done

--- 201525-lua/aoc.lua
+++ 201525-lua/aoc.lua
@@ -1,1 +1,18 @@
-print("Hello World")
+function findCodeAt(targetX, targetY)
+    code = 20151125
+    index = 2
+
+    while true do
+        for x=1,index do
+            y = index - x + 1
+            code = code * 252533 % 33554393
+            if x == targetX and y == targetY then
+                return code
+            end
+        end
+        index = index + 1
+    end
+end
+
+result = findCodeAt(3083, 2978)
+print(result)
